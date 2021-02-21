034b169aa7e4d64f837db5079552088a11787878
Eduardo Garza <omaarg@gmail.com>
2021-02-21 16:21:09 -0600

    Create multiple csv based on period

--- banorte_records_csv.py
+++ banorte_records_csv.py
@@ -8,6 +8,7 @@ import dataclasses
 from decimal import Decimal
 from datetime import datetime
 from pytz import timezone
+from collections import defaultdict
 from banes import banorte_email
 from banes import records
 
@@ -24,31 +25,6 @@ def calculate_total_amount(record):
     return Decimal(record.amount) + _extra_amount_total
 
 
-def read_record_from_email(json_email):
-    email_content = json.loads(json_email.read())
-    email_id = email_content['id']
-    email_from = email_content['from']
-    inbox_timestamp = email_content['internal_date']
-    inbox_date = datetime.fromtimestamp(inbox_timestamp / 1000.0, tz=timezone('America/Mexico_City')).isoformat()
-    email_body = str(base64.urlsafe_b64decode(email_content['body'].encode('ascii')), 'utf-8')
-    record = banorte_email.scrape(email_body)
-    result = {
-        'email_id': email_id,
-        'type': record.type,
-        'source': record.source,
-        'total_amount': calculate_total_amount(record),
-        'note': record.note,
-        'inbox_timestamp': inbox_timestamp,
-        'inbox_date': inbox_date,
-        'operation_date': record.operation_date,
-        'application_date': record.application_date if record.type == records.EXPENSE_RECORD_TYPE else None,
-        'from': email_from,
-        'raw': json.dumps(dataclasses.asdict(record), default=str),
-    }
-
-    return result, email_body
-
-
 def make_csv_row(record, email_metadata):
     return dict({
         'type': record.type,
@@ -61,16 +37,7 @@ def make_csv_row(record, email_metadata):
     }, **email_metadata)
 
 
-def main():
-    input_folder = sys.argv[1]
-    p = pathlib.Path(input_folder)
-
-    try:
-        os.mkdir('failures')
-    except FileExistsError:
-        pass
-
-    records_csv = open('records.csv', 'w', newline='')
+def make_period_csv(period, rows):
     fieldnames = [
         'type',
         'source',
@@ -84,37 +51,62 @@ def main():
         'email_timestamp',
         'email_date',
     ]
-    csv_writer = csv.DictWriter(records_csv, fieldnames=fieldnames)
 
-    csv_writer.writeheader()
+    with open(f'period_records/{period}-{len(rows)}.csv', 'w', newline='') as records_csv:
+        csv_writer = csv.DictWriter(records_csv, fieldnames=fieldnames)
 
-    for path in p.glob(f'*.json'):
+        csv_writer.writeheader()
+
+        for row in rows:
+            csv_writer.writerow(row)
+
+
+def main():
+    input_folder = sys.argv[1]
+
+    try:
+        os.mkdir('failures')
+    except FileExistsError:
+        pass
+
+    try:
+        os.mkdir('period_records')
+    except FileExistsError:
+        pass
+
+    period_csv_rows = defaultdict(list)
+
+    for path in pathlib.Path(input_folder).glob(f'*.json'):
         with path.open() as json_email:
 
             try:
                 email_content = json.loads(json_email.read())
                 email_body = str(base64.urlsafe_b64decode(email_content['body'].encode('ascii')), 'utf-8')
-                record = banorte_email.scrape(email_body)
                 email_id = email_content['id']
                 email_timestamp = email_content['internal_date']
+                date = datetime.fromtimestamp(email_timestamp / 1000.0, tz=timezone('America/Mexico_City'))
                 email_metadata = {
                     'email_id': email_id,
                     'email_from': email_content['from'],
                     'email_timestamp': email_timestamp,
-                    'email_date': datetime.fromtimestamp(email_timestamp / 1000.0,
-                                                         tz=timezone('America/Mexico_City')).isoformat(),
+                    'email_date': date.isoformat(),
                 }
+                record = banorte_email.scrape(email_body)
                 if record.type is not records.ACCOUNT_OPERATION_TYPE:
+                    period = f'{date.year}-{date.month}'
                     row = make_csv_row(record, email_metadata)
 
-                    csv_writer.writerow(row)
+                    period_csv_rows[period].append(row)
+
             except Exception as e:
                 print(f'{email_id} - {e}')
 
                 with open(f'./failures/{email_id}.html', 'w') as email_file:
                     email_file.write(email_body)
 
-    records_csv.close()
+    for period, csv_rows in period_csv_rows.items():
+        print(period)
+        make_period_csv(period, csv_rows)
 
 
 if __name__ == '__main__':
